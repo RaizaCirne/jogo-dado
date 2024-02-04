0590a944fc255d2caa09db0cc64911822f940a06
Raiza Cirne <raizacirne@gmail.com>
2024-02-04 19:07:23 -0300

    Feat: add projeto dado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,27 @@
 let sorteio = parseInt(Math.random() * 6 + 1);
 
 function alteraImg() {
-  if (sorteio === 1) {
-    document.getElementById('imagem-dado').src='assets/images/face1.png';
-} else if (sorteio === 2) {
-  document.getElementById('imagem-dado').src='assets/images/face2.png';
-} else if (sorteio === 3) {
-  document.getElementById('imagem-dado').src='assets/images/face3.png';
-} else if (sorteio === 4) {
-  document.getElementById('imagem-dado').src='assets/images/face4.png';
-} else if (sorteio === 5) {
-  document.getElementById('imagem-dado').src='assets/images/face5.png';
-} else {
-  document.getElementById('imagem-dado').src='assets/images/face6.png';
+  switch(sorteio) {
+    case 1: 
+      document.getElementById('imagem-dado').src='assets/images/face1.png';
+      break; 
+    case 2: 
+      document.getElementById('imagem-dado').src='assets/images/face2.png';
+      break; 
+    case 3: 
+      document.getElementById('imagem-dado').src='assets/images/face3.png';
+      break;
+    case 4: 
+      document.getElementById('imagem-dado').src='assets/images/face4.png';
+     break;
+    case 5: 
+     document.getElementById('imagem-dado').src='assets/images/face5.png';
+      break;
+    case 6: 
+      document.getElementById('imagem-dado').src='assets/images/face6.png';
+      default:
+  }
 }
-}
 
-
-
-// const btn = document.getElementById('button');
-// btn.addEventListener('click', function () {
-//   alteraImg();
-// })
-
-switch(sorteio) {
-  case 1: 
-    alteraImg();
-    break; 
-  case 2: 
-    alteraImg();
-    break; 
-  case 3: 
-    alteraImg();
-    break;
-  case 4: 
-    alteraImg();
-   break;
-  case 5: 
-   alteraImg();
-    break;
-  case 6: 
-    alteraImg();
-    default:
-}
+var btn = document.getElementById('button');
+btn.addEventListener('click', alteraImg);
